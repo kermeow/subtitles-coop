81518413519d620c12d520260a0a8ab85e05973a
jayden <46307433+kermeow@users.noreply.github.com>
2025-01-03 04:50:28 +0000

    feat: english voice subtitles

---
+++ src/b_subtitles/en.lua
@@ -0,0 +1,48 @@
+local _ = [[
+
+     ______ _   _  _____ _      _____  _____ _    _
+    |  ____| \ | |/ ____| |    |_   _|/ ____| |  | |
+    | |__  |  \| | |  __| |      | | | (___ | |__| |
+    |  __| | . ` | | |_ | |      | |  \___ \|  __  |
+    | |____| |\  | |__| | |____ _| |_ ____) | |  | |
+    |______|_| \_|\_____|______|_____|_____/|_|  |_|
+
+]]
+
+local id = add_subtitle_set("English", "kermeow", "en")
+
+do -- VOICE
+    add_subtitle(id, "VOICE", "YAH", "Yah!")
+    add_subtitle(id, "VOICE", "WAH", "Wah!")
+    add_subtitle(id, "VOICE", "HOO", "Hoo!")
+    add_subtitle(id, "VOICE", "HOOHOO", "Hoohoo!")
+    add_subtitle(id, "VOICE", "YAHOO", "Yahoo!")
+    add_subtitle(id, "VOICE", "UH", "Uhh!")
+    add_subtitle(id, "VOICE", "UH2", "Uh!")
+    add_subtitle(id, "VOICE", "HRMM", "*retches*")
+    add_subtitle(id, "VOICE", "WHOA", "Woah!")
+    add_subtitle(id, "VOICE", "EEUH", "Eeeuh.")
+    add_subtitle(id, "VOICE", "ATTACKED", "*groans*")
+    add_subtitle(id, "VOICE", "OOOF", "Ooof.")
+    add_subtitle(id, "VOICE", "HERE_WE_GO", "Here we go!")
+    add_subtitle(id, "VOICE", "YAWNING", "*yaaawn*")
+    add_subtitle(id, "VOICE", "SNORING", "*snores*")
+    add_subtitle(id, "VOICE", "WAAAOOOW", "WAAAOOOW!")
+    add_subtitle(id, "VOICE", "HAHA", "Haha!")
+    add_subtitle(id, "VOICE", "ON_FIRE", "Hahahahahahahaaaah!")
+    add_subtitle(id, "VOICE", "DYING", "*moans*")
+    add_subtitle(id, "VOICE", "PANTING", "Hooh.")
+    add_subtitle(id, "VOICE", "COUGHING", "*cough*")
+    add_subtitle(id, "VOICE", "MAMA_MIA", "Mamma mia!")
+    add_subtitle(id, "VOICE", "OKEY_DOKEY", "Okey dokey!")
+    add_subtitle(id, "VOICE", "DROWNING", "*bllp blp bluh*")
+    add_subtitle(id, "VOICE", "WAHA", "Wahaah!")
+    add_subtitle(id, "VOICE", "YIPPEE", "Yippee!")
+    add_subtitle(id, "VOICE", "DOH", "Doh.")
+    -- add_subtitle(id, "VOICE", "GAME_OVER", "Game over!")
+    add_subtitle(id, "VOICE", "HELLO", "Hello!")
+    -- add_subtitle(id, "VOICE", "PRESS_START_TO_PLAY", "Press START to play.")
+    add_subtitle(id, "VOICE", "TWIRL_BOUNCE", "Boing!")
+    add_subtitle(id, "VOICE", "SO_LONGA_BOWSER", "So longa, Bowser!")
+    add_subtitle(id, "VOICE", "IMA_TIRED", "I'ma tired!")
+end
